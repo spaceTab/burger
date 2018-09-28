15c9dc8b4397307dacd31e17da610866adef0bbb
spaceTab <93plaistj@protonmail.ch>
2018-09-28 15:27:01 -0400

    updated connections, unknown changes to index >.<

--- config/connections.js
+++ config/connections.js
@@ -1,17 +1,23 @@
 require('dotenv').config()
 const mysql = require("mysql");
-
+const connection;
 //creating db for burger_db
-const connect = mysql.createConnection({
-    host:     "localhost",
-    port:     "3306", 
-    user:     "root", 
-    //password: process.env.DB_PASS,
-    password: "SuperSecretPasswordHere",
-    database: "burger_db" 
-});
 
-connect.connect( error => {
+=
+
+if (process.env.JAWSDB_URL) {
+    connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connect = mysql.createConnection({
+        host:     "localhost",
+        port:     "3306", 
+        user:     "root", 
+        password: "SuperSecretPasswordHere",
+        database: "burger_db" 
+    });
+}
+
+connection.connect( error => {
     if (error) throw error;
 
     console.log(`connection made => ${connect.threadId}`);
