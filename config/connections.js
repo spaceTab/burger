a6fad6773ddaa7a2329881f3d6ad727e7d8378d2
spaceTab <93plaistj@protonmail.ch>
2018-10-14 14:18:12 -0400

    connection fix again

--- config/connections.js
+++ config/connections.js
@@ -4,7 +4,7 @@ let connection;
 //creating db for burger_db
 
 if (process.env.JAWSDB_URL) {
-    connection.mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host:     "localhost",
